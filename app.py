40ab8906666bd4a822dbee86a8cc01d16d447603
Stefan <stefanbfritz@gmail.com>
2020-05-22 15:18:41 -0400

    add PATCH to /actors and /movies

--- app.py
+++ app.py
@@ -77,25 +77,38 @@ def add_actor():
         'total_actors': len(Actor.query.all())
     })
 
-# @app.route('/actors/<int:actor_id>', methods=['PATCH'])
-# def modify_actor(actor_id):
-#     actor = Actor.query.filter(Actor.id=actor_id).one_or_none()
-#     if actor is None:
-#         abort(404)
-    
-#     body = request.get_json()
-#     name = body['name']
-#     birthdate = body['birthdate']
-#     gender = body['gender']
-#     seeking_work = bool(body['seekingWork'])
 
-#     try:
+@app.route('/actors/<int:actor_id>', methods=['PATCH'])
+def edit_actor(actor_id):
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+    if actor is None:
+        abort(404)
 
+    data = request.get_json()
+    name = data.get('name', None)
+    birthdate = data.get('birthdate', None)
+    gender = data.get('gender', None)
+    seeking_work = data.get('seekingWork', None)
+
+    try:
+        actor.name = name if name else actor.name
+        actor.dirthdate = birthdate if birthdate else actor.birthdate
+        actor.gender = gender if gender else actor.gender
+        actor.seeking_work = bool(seeking_work) if seeking_work \
+            else actor.seeking_work
+        actor.update()
+    except Exception:
+        abort(422)
+
+    return jsonify({
+        'success': True,
+        'updated_actor': actor.format()
+    })
 
 
 @app.route('/movies')
 def get_movies():
-    movies = Movie.query.order_by(Movie.title).all()
+    movies = Movie.query.order_by(Movie.genre).all()
     seleted_movies = get_paginated(request, movies)
 
     if len(seleted_movies) == 0:
@@ -130,6 +143,31 @@ def add_movie():
         'success': True,
         'id': new_movie.id,
         'total_movies': len(Movie.query.all())
+    })
+
+
+@app.route('/movies/<int:movie_id>', methods=['PATCH'])
+def edit_movie(movie_id):
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+    if movie is None:
+        abort(404)
+
+    data = request.get_json()
+    title = data.get('title', None)
+    release_date = data.get('releaseDate', None)
+    genre = data.get('genre', None)
+
+    try:
+        movie.title = title if title else movie.title
+        movie.release_date = release_date if release_date else movie.release_date
+        movie.genre = genre if genre else movie.genre
+        movie.update()
+    except Exception:
+        abort(422)
+    
+    return jsonify({
+        'success': True,
+        'updated_movie': movie.format()
     })
 
 
